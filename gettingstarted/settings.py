ec4daa8829542bf1874b94c5945bbcf5a2de5190
gaurav1792 <gd.jiit@gmail.com>
2015-04-01 13:48:11 +0530

    test4

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -1,33 +1,26 @@
 """
-Django settings for gdchat project.
+Django settings for gettingstarted project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
-from .email_info import *
-
-#for gmail or gmail apps
-EMAIL_USE_TLS = EMAIL_USE_TLS
-EMAIL_HOST = EMAIL_HOST
-EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-EMAIL_PORT = EMAIL_PORT
-
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lxy7(!4==6&go8sb=$n+wny3m1h$mia49+&x=*8-pg39kq367i'
+DEFAULT_SECRET_KEY = '3iy-!-d$!pc_ll$#$elg&cpr@*tfn-d5&n9ag=)%#()t$$5%5^'
+SECRET_KEY = os.environ.get('SECRET_KEY', DEFAULT_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
+    'hello'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -54,18 +47,17 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'gdchat.urls'
+ROOT_URLCONF = 'gettingstarted.urls'
 
-WSGI_APPLICATION = 'gdchat.wsgi.application'
+WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -73,22 +65,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'chat/static')
+
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'templates'),
-)
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'chat/static/uploads')
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'utc'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -97,9 +80,26 @@ USE_L10N = True
 USE_TZ = True
 
 
-
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- hello/models.py
+++ hello/models.py
@@ -1,23 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-import datetime
-import hashlib
-
-
-class Message(models.Model):
-    to_user = models.CharField(max_length=30)
-    from_user = models.CharField(max_length=30)
-    msg = models.TextField()
-    msg_type = models.CharField(max_length=10,default='text')
-    msg_date = models.DateTimeField(auto_now_add=True, blank=True)
-
-
-class Login_status(models.Model):
-    user = models.CharField(max_length=30)
-    is_online=models.BooleanField(default=0)
-    last_seen = models.DateTimeField(auto_now_add=True, blank=True)
-
-
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 # Create your models here.
+class Greeting(models.Model):
+    when = models.DateTimeField('date created', auto_now_add=True)
